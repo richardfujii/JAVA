295a332087d63c8c7d87a0bd0f5365caf8b984ac
unknown <richardcf1998@gmail.com>
2021-05-11 04:31:51 -0300

    Entity

---
+++ blogPessoal/src/main/java/org/generation/blogPessoal/model/Tema.java
@@ -0,0 +1,13 @@
+package org.generation.blogPessoal.model;
+
+import java.util.List;
+
+public class Tema {
+	
+	private long id;
+	
+	private String descricao;
+	
+	private List<Postagem> postagem;
+
+}
